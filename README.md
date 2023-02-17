7bfbcc99f4064d42375dd7c4ab8f02d9f7693ee0
clogan680 <clogan680@gmail.com>
2023-02-17 09:18:52 -0800

    initial sync

--- README.md
+++ README.md
@@ -1,1 +1,5 @@
 # M365_distro_list_mail_trace
+
+To run, ensure you have the powershell exchange online module enabled and you are an admin of the Microsoft tenant you will sign in with. 
+
+Distro lists that did not receive any mail will still have an entry in the csv, and it will just state "No Results" in all columns except for the name of the group.
---
+++ message_trace_groups.ps1
@@ -0,0 +1,43 @@
+﻿Import-Module ExchangeOnlineManagement
+
+# Connect to Microsoft 365
+Connect-ExchangeOnline
+
+# Get all distribution groups in Microsoft 365
+$groups = Get-DistributionGroup
+
+# Initialize an array to store the results
+$results = @()
+
+$getdate = Get-Date
+Write-Host('start date - ',$getdate.AddDays(-10))
+Write-Host('end date - ',$getdate)
+
+# Loop through each group
+foreach ($group in $groups) {
+   Write-Host($Group.PrimarySmtpAddress)
+   $traceresults = Get-MessageTrace -RecipientAddress $group.PrimarySmtpAddress -StartDate $getdate.AddDays(-10) -EndDate $getdate
+   if ($traceresults.length -eq 0) {
+   $results += New-Object PSObject -Property @{
+            Group = $group.DisplayName
+            Sender = "No Results"
+            Recipient = "No Results"
+            Subject = "No Results"
+            Date = "No Results"
+        }
+   }else {
+   foreach ($traceresult in $traceresults) {
+        # Store the relevant information in the results array
+        $results += New-Object PSObject -Property @{
+            Group = $group.DisplayName
+            Sender = $traceresult.SenderAddress
+            Recipient = $traceresult.RecipientAddress
+            Subject = $traceresult.Subject
+            Date = $traceresult.Received
+        }
+    }
+   }
+}
+
+# Export
+$results | Export-Csv -Path ".\MessageTraceResults.csv" -NoTypeInformation
