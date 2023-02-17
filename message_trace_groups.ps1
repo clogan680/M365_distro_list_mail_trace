825b5efafba0ee5ea38009d164e26e8492b53738
clogan680 <clogan680@gmail.com>
2023-02-17 10:27:34 -0800

    logging empty groups

--- message_trace_groups.ps1
+++ message_trace_groups.ps1
@@ -10,34 +10,43 @@ $groups = Get-DistributionGroup
 $results = @()
 
 $getdate = Get-Date
-Write-Host('start date - ',$getdate.AddDays(-10))
-Write-Host('end date - ',$getdate)
+
+#initialize counter for groups with no mail results
+$emptycount = 0
 
 # Loop through each group
 foreach ($group in $groups) {
-   Write-Host($Group.PrimarySmtpAddress)
-   $traceresults = Get-MessageTrace -RecipientAddress $group.PrimarySmtpAddress -StartDate $getdate.AddDays(-10) -EndDate $getdate
-   if ($traceresults.length -eq 0) {
-   $results += New-Object PSObject -Property @{
-            Group = $group.DisplayName
-            Sender = "No Results"
-            Recipient = "No Results"
-            Subject = "No Results"
-            Date = "No Results"
-        }
-   }else {
-   foreach ($traceresult in $traceresults) {
-        # Store the relevant information in the results array
+    Write-Host($Group.PrimarySmtpAddress)
+    $traceresults = Get-MessageTrace -RecipientAddress $group.PrimarySmtpAddress -StartDate $getdate.AddDays(-10) -EndDate $getdate
+    if ($traceresults.length -eq 0) {
+        $emptycount ++
         $results += New-Object PSObject -Property @{
-            Group = $group.DisplayName
-            Sender = $traceresult.SenderAddress
-            Recipient = $traceresult.RecipientAddress
-            Subject = $traceresult.Subject
-            Date = $traceresult.Received
+            Group     = $group.DisplayName
+            Sender    = "No Results"
+            Recipient = "No Results"
+            Subject   = "No Results"
+            Date      = "No Results"
         }
     }
-   }
+    else {
+        foreach ($traceresult in $traceresults) {
+            # Store the relevant information in the results array
+            $results += New-Object PSObject -Property @{
+                Group     = $group.DisplayName
+                Sender    = $traceresult.SenderAddress
+                Recipient = $traceresult.RecipientAddress
+                Subject   = $traceresult.Subject
+                Date      = $traceresult.Received
+            }
+        }
+    }
 }
+
+# log results
+Write-Host('Total number of groups found in tenant - ', $groups.length)
+Write-Host('Groups with no mail - ', $emptycount)
+Write-Host('start date - ', $getdate.AddDays(-10))
+Write-Host('end date - ', $getdate)
 
 # Export
 $results | Export-Csv -Path ".\MessageTraceResults.csv" -NoTypeInformation
